5067e47764a814eadb0175edf3d37cbc18bb6a83
chancho1erick <erickrodolfodavidalvarescortes@gmail.com>
2026-01-22 01:13:39 -0600

    Refactor agregarAlCarrito to agregarCarrito
    Refactor shopping cart functions and replace total with carrito array.

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-let total = 0;
+let carrito = [];
 
-function agregarAlCarrito(nombre, precio) {
-  const lista = document.getElementById("listaCarrito");
-  const item = document.createElement("li");
-  item.textContent = `${nombre} - $${precio}`;
-  lista.appendChild(item);
-
-  total += precio;
-  document.getElementById("total").textContent = total;
+function agregarCarrito(producto) {
+  carrito.push(producto);
+  alert("🛒 Agregado al carrito: " + producto);
 }
